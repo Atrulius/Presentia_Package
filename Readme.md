d1bc4262e1a60430197602f6309efdc79387027c
Tobias E.S <tobias.stokkenes@gmail.com>
2023-05-15 12:32:12 +0200

    Add project files.

---
+++ Readme.md
@@ -0,0 +1,13 @@
+This is a package for C# Windows Forms
+
+This package helps you use the file extensions:
+.ptia (Short for Presentia) and
+.ptie (.ptia but E for Editor)
+
+This package has two main namespaces; "Type", and "Convert"
+
+Type is the type of presentia that you want. You can have the editor version,
+view only, and you can also have them as they are when they get exported as a file.
+
+Convert lets you convert between different types. So for example, if you want to
+export an editor version of the presentation, you can convert from PTIE to PTIA file.
---
+++ Type.cs
@@ -0,0 +1,85 @@
+﻿using NAudio.Wave;
+
+namespace Presentia.Type {
+
+    public class PTIA {
+
+    }
+
+    public class PTIA_File {
+
+        public List<byte[]> images_as_byte = new List<byte[]>();
+
+        public List<int> times = new List<int>();
+
+        public byte[] sound;
+
+        public void Add_Image( Image Image, int Time ) {
+
+            ImageConverter converter = new ImageConverter();
+            byte[] arr = (byte[])converter.ConvertTo(Image, typeof(byte[]));
+
+            images_as_byte.Add( arr );
+
+            times.Add( Time );
+        }
+
+        public void Set_Sound( byte[] Sound ) {
+
+            sound = Sound;
+        }
+    }
+
+    public class PTIE {
+
+        public List<string> Imports = new List<string>();
+
+        public List<Image> Images = new List<Image>();
+
+        public class Image {
+
+            public Image( System.Drawing.Image The_Image, int The_Import_Index, int The_Time ) {
+
+                Drawing = The_Image;
+
+                Import = The_Import_Index;
+
+                Time = The_Time;
+            }
+
+            public System.Drawing.Image Drawing;
+
+            public int Import;
+
+            public int Time;
+        }
+
+        public Mp3FileReader Sound_Reader;
+        public WaveOutEvent Sound_Output = new WaveOutEvent();
+
+        public int? Sound_Import;
+    }
+
+    public class PTIE_File {
+
+        public List<string> Imports = new List<string>();
+
+        public int Sound_Import;
+
+        public List<Image> Images = new List<Image>();
+
+        public class Image {
+
+            public Image( int The_Import_Index, int The_Time ) {
+
+                Import = The_Import_Index;
+
+                Time = The_Time;
+            }
+
+            public int Import;
+
+            public int Time;
+        }
+    }
+}
